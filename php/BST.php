4a1509506cf04bcc5a25e06837b6acd5faecf442
Phlare <phlarebot@gmail.com>
2015-07-11 21:34:33 -0700

    added remove to php BST

--- php/BST.php
+++ php/BST.php
@@ -24,8 +24,19 @@ class BST {
 		return $this->_count;
 	}
 
+	// public insert function
+	// this can be configured to use recursive insertion or not
+	// TODO: possibly extend this option to be set at the creation of tree insted of on insert
+	public function insert($key, $data=null, $recursive=false) {
+		if ($recursive) {
+			return $this->_recursiveInsert($key, $data, $this->_root);
+		} else {
+			return $this->_insert($key, $data);
+		}
+	}
+
 	// push new node to BST
-	public function insert($key, $data=null) {
+	protected function _insert($key, $data=null, $root=null) {
 
 		if ($this->_root === null) {
 			// tree is empty, just create a node and point _root there.
@@ -58,22 +69,124 @@ class BST {
 		}
 	}
 
-	// remove a node by key
-	public function remove($key) {
+	protected function _recursiveInsert($key, $data=null, &$root=null) {
+		if (!$root) {
+			// make the new node
+			$root = $newNode = new BSTNode($key, $data);
+			$this->_count ++;
+			return $newNode;
+		} elseif ($key === $root->key) {
+			// key already exists.
+			return $root;
+		} elseif ($key > $root->key) {
+			// return new BSTNode($key, $data, $root->left, $this->insert($key, $data, $root->right));
+			return $this->_recursiveInsert($key, $data, $root->right);
 
+		} elseif ($key < $root->key) {
+			// return new BSTNode($key, $data, $this->insert($key, $data, $root->left), $root->right);
+			return $this->_recursiveInsert($key, $data, $root->left);
+		}
+	}
+
+	// attempts to find and remove a node.  returns true on successfull removal
+	public function remove($key) {
+		// iterate through, and find the key
+		$direction = null;  // keep track of the direction we just traversed
+		$current = $this->_root;
+		$parent = null;
+		while ($current !== null) {
+			if ($key > $current->key) {
+				if ($current->right) {
+					$parent = $current;
+					$current = $current->right;
+					$direction = 'right';
+				} else {
+					return false;
+				}
+			} elseif ($key < $current->key) {
+				if ($current->left) {
+					$parent = $current;
+					$current = $current->left;
+					$direction = 'left';
+				} else {
+					return false;
+				}
+			} else {
+				// key matches
+				if ($current->right && $current->left) {
+					// node has both children. we need to do the following:
+					// find the leftmost descendant of the immediate right child
+					// copy it's key and data to the current node, and delete the original child
+					$successorData = $this->_getMinChild($current->right);
+					// now that we have it's data, delete the successor
+					$this->remove($successorData->key);
+					// set the values from the successor to the node to be deleted
+					$current->key = $successorData->key;
+					$current->data = $successorData->data;
+					// clean up that temporary node
+					unset($successorData);
+					return true;
+				} elseif ($current->left) {
+					// node has just left
+					if ($parent && $direction) {
+						$parent->$direction = $current->left;
+					} else {
+						$this->_root = $current->left;
+					}
+				} elseif ($current->right) {
+					// node has just right
+					if ($parent && $direction) {
+						$parent->$direction = $current->right;
+					} else {
+						$this->_root = $current->right;
+					}
+				} else {
+					// node has neither child
+					if ($parent && $direction) {
+						$parent->$direction = null;
+					} else {
+						// this is the only node in the tree
+						$this->_root = null;
+					}
+				}
+				$this->_count--;
+				return true;
+			}
+		}
+	}
+
+	// finds the leftmost descendant, deletes it, and returns it's key and data
+	protected function _getMinChild(&$root=null) {
+		// keep iterating left until we can't, keeping track of parent
+		$current = $root;
+		$parent = null;
+		while ($current->left) {
+			$parent = $current;
+			$current = $current->left;
+		}
+		// return a new node with this data
+		return new BSTNode($current->key, $current->data);
 	}
 
 	// just using print_r on the root for now
-	public function display() {
+	public function display($type='print_r') {
 
-		$printData = array(
-			'count' => $this->_count,
-			'BST' => $this->_root
-		);
+		if (!$this->_root) {
+			echo "this tree is empty" . "\n";
+			return;
+		}
 
-		print_r($printData);
+		if ($type === 'pretty') {
+			// TODO: some way to dynamically print
+		} else {
+			$printData = array(
+				'count' => $this->_count,
+				'BST' => $this->_root
+			);
+
+			print_r($printData);
+		}
 	}
-
 
 }
 
@@ -82,15 +195,15 @@ class BST {
  */
 class BSTNode {
 	public $key;
+	public $data;
 	public $left;
 	public $right;
-	public $data;
 
 	public function __construct($key, $data=null, $left=null, $right=null) {
 		$this->key = $key;
+		$this->data = $data;
 		$this->left = $left;
 		$this->right = $right;
-		$this->data = $data;
 	}
 
 	// gets the data in a particular node
--- php/tests/TestBST.php
+++ php/tests/TestBST.php
@@ -33,18 +33,46 @@ class testBST {
 		}
 
 		$existingKeys = array();
-		for ($i = 1; $i <= 15; $i++) {
-			do {
-				// keep trying until we have a unique key
-				$key = rand(1,99);
-			} while (in_array($key, $existingKeys));
+		$bst->insert(40, '40');
+		$bst->insert(20, '20');
+		$bst->insert(60, '60');
+		$bst->insert(10, '10');
+		$bst->insert(30, '30');
+		$bst->insert(50, '50');
+		$bst->insert(70, '70');
+		$bst->insert(90, '90');
+		$bst->insert(15, '15');
+		$bst->insert(45, '45');
+		$bst->insert(55, '55');
+		$bst->insert(35, '35');
+		$bst->insert(25, '25');
+		$bst->insert(75, '75');
+		$bst->insert(5, '5');
+		// for ($i = 1; $i <= 15; $i++) {
+		// 	do {
+		// 		// keep trying until we have a unique key
+		// 		$key = rand(1,99);
+		// 	} while (in_array($key, $existingKeys));
 
-			$bst->insert($key);
-			$existingKeys[] = $key;
+		// 	$bst->insert($key);
+		// 	$existingKeys[] = $key;
+		// }
+		if ($bst->getCount() !== 15) {
+			self::printError("problem with insertion. should be 15 keys");
 		}
 
 		// display... for now
 		$bst->display();
+
+		// test remove
+		$result = $bst->remove(50);
+		$result = $bst->remove(70);
+
+
+		// display... for now
+		$bst->display();
+
+
 	}
 
 	public static function printError($message) {
