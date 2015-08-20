a28498df1521a35ab1e27fc56c2e78d88550f473
Phlare <phlarebot@gmail.com>
2015-08-20 13:10:16 -0700

    coin sums

--- js/COW/coinSums.js
+++ js/COW/coinSums.js
@@ -5,15 +5,17 @@
 // Assume the denominations up to $5 (in cents, that’s 1, 5, 10, 25, 50, 100, 500).
 // For example, an input of 10 would output 4 (10x1, 5x2, 5x1+1x5, 1x10).
 var CoinSum = function() {
-  this.combinations = 0;
-  this.tailPointers = [];
+  this.totalCombinations = 0;
+  this.comboHashes = [];
+  this.combinations = [];
 };
 
-var CoinSumNode = function(total, step, value, children) {
+var CoinSumNode = function(total, step, value, parent) {
   this.total = total || 0;
   this.step = step || 0;
   this.value = value || null;
-  this.children = children || [];
+  this.parent = parent || null;
+  this.children = {};
 
   return this;
 };
@@ -23,11 +25,16 @@ CoinSum.prototype = {
   init: function(data) {
     return this;
   },
-  denoms: [1,5,10,25,50,100,500],
+  denoms: [500,100,50,25,10,5,1],
+  clearCombinations: function() {
+    this.totalCombinations = 0;
+    this.comboHashes = [];
+    this.combinations = [];
+  },
   // find the set of combinations that total the given number
   findCombinations: function(total) {
     // clear the previous count if necessary
-    this.combinations = 0;
+    this.clearCombinations();
     console.log('----------------------------');
     console.log('finding Combinations for ' + total);
 
@@ -39,8 +46,9 @@ CoinSum.prototype = {
     }
     total = Math.round(total) || 0;
     // now find combinations.
-    var combinations = (total === 0) ? 0 : this.calculateCombinations(total,0, null);
+    var combinations = this.calculateCombinations(total,0, null);
     console.log(combinations + ' combinations found.');
+    console.log(this.combinations);
     return combinations;
 
   },
@@ -48,13 +56,12 @@ CoinSum.prototype = {
     var node = new CoinSumNode(total, step, value);
     this.addChildren(total, step, node);
 
-    console.log(node);
     // return number of combinations
-    return this.combinations;
+    return this.totalCombinations;
   },
   addChildren: function(total, step, node) {
-    console.log('adding children');
-    console.log({total:total, step:step});
+    // console.log('adding children');
+    // console.log({total:total, step:step});
     var len = this.denoms.length;
     // loop through the possible denominations
     for (var i = 0; i < len; i++) {
@@ -62,14 +69,42 @@ CoinSum.prototype = {
       var newTotal = total - this.denoms[i];
       if (newTotal >= 0) {
         // make the child node and attach to this node in children array
-        console.log('adding new child of value ' + this.denoms[i] + ' at step ' + (step + 1));
-        var child = new CoinSumNode(newTotal, step+1, this.denoms[i]);
-        node.children.push(child);
-        if (newTotal == 0) {
-          this.combinations += 1;
+        // we need to make sure we're not just creating a new permutation of the same steps though.
+        var child = new CoinSumNode(newTotal, step+1, this.denoms[i], node);
+        var comboHash = this.checkComboHash(child);
+        if (comboHash) {
+          // console.log('adding new child of value ' + this.denoms[i] + ' at step ' + (step + 1));
+
+          // if new total is 0, then we've found a full combination
+          if (newTotal == 0) {
+            this.combinations.push(comboHash);
+            this.totalCombinations += 1;
+          } else {
+            // otherwise, recursively add children to this node
+            this.addChildren(newTotal, step+1, child);
+          }
+          node.children[this.denoms[i]] = child;
         }
       }
     }
+  },
+  checkComboHash: function(node) {
+    // create an array of the items used, then sort them and create a hash we can use to compare
+    var steps = [];
+    var current = node;
+    while (current != null && typeof current.value != 'undefined' && current.value != null) {
+      steps.push(current.value);
+      current = current.parent;
+    }
+    // sort this array and make a "hash" of it to compare with other nodes already created
+    // this will reduce our paths to combinations instead of permutations
+    var stepHash = steps.sort().reverse().join('-');
+    if (this.comboHashes.indexOf(stepHash) === -1) {
+      // doesn't already exist.   add it to existing combinations and return true
+      this.comboHashes.push(stepHash);
+      return stepHash;
+    }
+    return false;
   },
   // just to standardize errors.
   complain: function(message) {
